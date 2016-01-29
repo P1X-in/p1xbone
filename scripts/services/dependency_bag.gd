ea4deb801ce5641dbfeb08f12b06058a2bc2bf58
WermooZ <p.weremczuk@gmail.com>
2016-01-29 19:59:54 +0100

    game structure

---
+++ scripts/services/dependency_bag.gd
@@ -0,0 +1,7 @@
+var root
+
+var helpers = preload("res://scripts/services/helpers.gd").new()
+var file_handler = preload('res://scripts/services/file_handler.gd').new()
+
+func init_root(root_node):
+    self.root = root_node
